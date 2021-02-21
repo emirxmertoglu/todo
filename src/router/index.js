4b94e326fa0047445f7790c9f6bb4048290b7d1e
emirxmertoglu <emirxmertoglu@outlook.com>
2021-02-21 14:23:21 +0300

    edited index.js for new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from '../firebase'
 
 import BoardList from '@/components/BoardList'
 import ShowBoard from '@/components/ShowBoard'
 import AddBoard from '@/components/AddBoard'
 import EditBoard from '@/components/EditBoard'
 import NotFound from '@/components/NotFound'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
     path: '/',
     name: 'BoardList',
-    component: BoardList
+    component: BoardList,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/show-board/:id',
     name: 'ShowBoard',
-    component: ShowBoard
+    component: ShowBoard,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/add-board',
     name: 'AddBoard',
-    component: AddBoard
+    component: AddBoard,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/edit-board/:id',
     name: 'EditBoard',
-    component: EditBoard
+    component: EditBoard,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '*',
@@ -42,5 +67,20 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  const currentUser = await firebase.getCurrentUser()
+  const requiresAuth = to.matched.some(
+    (record) => record.meta.requiresAuth
+  );
+  if (requiresAuth && !currentUser) {
+    next("login");
+  } else if (!requiresAuth && currentUser) {
+    next('BoardList')
+  }
+  else {
+    next();
+  }
+});
 
 export default router
